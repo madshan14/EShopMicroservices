c878315d196da65ff592a8f22103e3811f414a32
madshan14 <madshanofficial@gmail.com>
2024-08-06 03:26:42 +0800

    updatex

---
+++ src/Services/Catalog/WebApplication1/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description,string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler(IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Create Product Entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            //Save to DB
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            //Return result
+            return new CreateProductResult(product.Id);
+
+        }
+    }
+}
